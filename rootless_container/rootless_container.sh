571900f9a33701c84e8a13f9199e628c001733b3
root <root@localhost.localdomain>
2025-06-12 00:00:34 -0400

    Removed progress output fromimage pull.

--- rootless_container/rootless_container.sh
+++ rootless_container/rootless_container.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-image="false"
-host_mount="false"
-while [[ $image != "true" ]]; do
+is_image="false"
+is_host_mount="false"
+container_name=""
+
+while [[ $is_image != "true" ]]; do
 	read -p "Enter image to run: " image
-	if [[ $image == "test" ]]; then
-		image="true"
-		while [[ $host_mount != "true" ]]; do
-			read -p "Enter the path of the host mount: " host_mount
-			if [[ $host_mount == "host" ]]; then
-				host_mount="true"
-				read -p "Enter the path of the container mount: " container_mount
-				echo "creating container now..."
+	if podman pull $image > /dev/null 2>&1; then
+		read -p "Enter name for container: " container_name
+		is_image="true"
+		while [[ $is_host_mount != "true" ]]; do
+			read -p "Enter the full path of the host mount: " host_mount
+			if [[ -d $host_mount ]]; then
+				is_host_mount="true"
 			else
 				echo "no directory"
+				mkdir -vp $host_mount 
+				is_host_mount="true"
 			fi
 		done
 	else
 		echo "no image"
 	fi
 done
+
+read -p "Enter the path of the container mount: " container_mount
+echo "creating container now...."
+podman run -d --name $container_name -v $host_mount:$container_mount $image
